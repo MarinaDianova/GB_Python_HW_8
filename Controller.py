fa84ddce829a902393e5782fd617f1efa612a5fc
Marina Dianova <m_di@mail.ru>
2023-05-22 22:13:38 +0300

    Controller update on work branch

--- Controller.py
+++ Controller.py
@@ -2,15 +2,15 @@ import view, model
 
 
 def start():
-    # view.greetings()
+    # view.greetings() - ЛИБО просто пишем как в строке ниже
+    print('1. вывод, 2. добавление, 3. поиск, 4. выход') 
+#"4. редактирование, 5. удаление"
     if answer == '1':
         view.show()
     elif answer == '2':
-        model.add_contant
-            
+        model.add_contact
     elif answer == '3':
         model.search()
-
     elif answer == '4':
         view.bye()
         break
