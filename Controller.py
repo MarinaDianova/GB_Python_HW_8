db9d539e739bcd41123375ca34127eab80055234
Marina Dianova <m_di@mail.ru>
2023-05-22 22:02:20 +0300

    Controller 1st update

---
+++ Controller.py
@@ -0,0 +1,19 @@
+import view, modulefinder
+
+
+def start():
+    # view.greetings()
+    if answer == '1':
+        view.show()
+    elif answer == '2':
+        model.add_contant
+            
+    elif answer == '3':
+        model.search()
+
+    elif answer == '4':
+        view.bye()
+        break
+
+    else:
+        model.error()
